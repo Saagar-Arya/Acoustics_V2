c5ca20a73daf9ce34ad310571f44e5a8ae7c2d2b
Saagar Arya <saagar.arya12@gmail.com>
2025-11-29 21:40:49 -0500

    big save

--- Scripts/Data_Controller.py
+++ Scripts/Data_Controller.py
@@ -1,27 +1,3 @@
-import os
-import matplotlib.pyplot as plt
-import time
-import Logic as LOGIC
-import Hydrophone_Array
-
-SAMPLING_FREQ = 781250
-SELECTED = [True, True, True, True]
-# what is the truth of the hydrophone
-prefix = "0_"
-data_collection = prefix + "Data_Collection"
-base_path = os.path.join("Scripts", data_collection)
-os.makedirs(base_path, exist_ok = True)
-
-logic = LOGIC.Logic(sampling_freq=SAMPLING_FREQ)
-logic.print_saleae_status()
-
-epochs = 100
-for epoch in range (0, epochs):
-    print(f"epoch {epoch}")
-    
-    time_stamp = time.strftime('%Y-%m-%d--%H-%M-%S')
-    name = prefix + time_stamp
-    logic.export_binary_capture(2, base_path, name)
-
-logic.kill_logic()
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b47b485baa23fb5110ccb641210b4c523df423d6a3b2f0150f63e04dcd66fcf
+size 691
--- Scripts/Data_Parser.py
+++ Scripts/Data_Parser.py
@@ -1,34 +1,3 @@
-import Hydrophone_Array
-import os
-SAMPLING_FREQ = 781250
-SELECTED = [False, True, True, True]
-
-hydrophone_array = Hydrophone_Array.HydrophoneArray(sampling_freq=SAMPLING_FREQ, enable_data_sample = True, data_sample_out_dir="Scripts/Test_set")
-
-base_paths = ["Scripts/0_Data_Collection","Scripts/2_Data_Collection"]
-
-for base_path in base_paths:
-    filenames = os.listdir(base_path)
-    for filename in filenames:
-        ext = os.path.splitext(filename)[1].lower()
-        if ext != ".bin":
-            continue
-
-        path = os.path.join(base_path,filename)
-        print(path)
-
-        hydrophone_array.load_from_path(path)
-
-        hydrophone_array.estimate_selected_by_envelope(SELECTED)
-        #hydrophone_array.plot_selected_envelope(SELECTED)
-        hydrophone_array.print_envelope_toas()
-        print("=" * 30)
-
-        hydrophone_array.estimate_selected_by_gcc(SELECTED)
-        #hydrophone_array.plot_selected_gcc(SELECTED)
-        hydrophone_array.print_gcc_tdoa(SELECTED)
-        print("=" * 30)
-
-        hydrophone_array.data_sample()
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:cee406d0e91201b45f483ecd040a363dee9de12e5b57ee26cfef0f3bdb2f00e3
+size 1089
