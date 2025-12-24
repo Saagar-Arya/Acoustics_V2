c3e36a3a7022f30ae04ffb3e7d9c4c7981ecc081
Ahaan1234 <1ahaanshah@gmail.com>
2025-12-24 14:33:00 +0530

    set H0 to 0.0, so that blank column doesn't cuse nn_train errors

--- Scripts/Hydrophone_Array.py
+++ Scripts/Hydrophone_Array.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41053ccb918b75be1c3cca9efa7ef0bb6364a830691622c08867041c637cd246
-size 35572
+oid sha256:afb2b790b1a3c5c2ddeddfb5ff443f5a157ec158ba4bb4d3e9a463a1bd2a8557
+size 35656
