073a37dea32d7778dfc8c13db166acc447e64a9f
Ahaan1234 <1ahaanshah@gmail.com>
2025-12-26 00:47:41 +0530

    in feats append, added code that uses 0.0 as value instead of empty string, to avoid math errors in nn_train

--- src/nn_train.py
+++ src/nn_train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79075028b1419509413fefdcb0930bb6942f5788dcafd03a29b65910153ea1d
-size 12262
+oid sha256:32ea566d6e221184dfab530ed18d7be349ab32ceef687b7fcbbf30e49c0983df
+size 12310
