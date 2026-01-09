e7c92211df0ba8f4e3450f84268d263f08a046df
Isabella Chen <ivc2@duke.edu>
2026-01-08 19:54:06 -0500

    PCB Data, first 10 hydrophone 0 is in front, next 10 hydrophone 2 is in front.

--- Scripts/Controller.py
+++ Scripts/Controller.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07a338100cbcbf050feadc5074226327737577b3a68ca5dfc2c47f9ee92a7226
-size 1938
+oid sha256:7ea8e26ced70e0bc27eb49d74dcc12708177c391c4b0891392d3f8c4e2553e14
+size 1919
